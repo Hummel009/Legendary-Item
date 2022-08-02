cfab4e0d4da2627462c4efa05a2996f559f4ab03
Hummel009 <emperorbur@gmail.com>
2022-08-02 22:47:53 +0300

    yes

--- src/main/java/legendarium/LIItemSword.java
+++ src/main/java/legendarium/LIItemSword.java
@@ -4,6 +4,6 @@ import net.minecraft.item.*;
 
 public class LIItemSword extends SwordItem {
 	public LIItemSword() {
-		super(ItemTier.NETHERITE, 12, 2F, new Properties().tab(LICreativeTabs.tabWeapons));
+		super(ItemTier.DIAMOND, 12, 2F, new Properties().tab(LICreativeTabs.tabWeapons));
 	}
 }
