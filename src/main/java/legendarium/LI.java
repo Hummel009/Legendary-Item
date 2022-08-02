f68241cb3e1bc481455339ae968539ddaefcffde
Hummel009 <emperorbur@gmail.com>
2022-08-02 20:52:02 +0300

    partial port

--- src/main/java/legendarium/LI.java
+++ src/main/java/legendarium/LI.java
@@ -8,6 +8,7 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.*;
 
 @Mod("legendarium")
