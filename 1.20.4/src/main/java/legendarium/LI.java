3d37ece6ab67e118e1add8c72428d6871589b3b0
Hummel009 <hummel.turbamentum@gmail.com>
2023-12-30 13:19:30 +0300

    131829122023

--- 1.20.4/src/main/java/legendarium/LI.java
+++ 1.20.4/src/main/java/legendarium/LI.java
@@ -21,7 +21,7 @@ import java.util.Collection;
 
 @Mod("legendarium")
 public class LI {
-	public static final String DISABLE_CURSEFORGE_DUPLICATE_NOTICE = "101129102023";
+	public static final String DISABLE_CURSEFORGE_DUPLICATE_NOTICE = "131829122023";
 
 	public static final LICommonProxy PROXY = DistExecutor.safeRunForDist(() -> LIClientProxy::new, () -> LICommonProxy::new);
 
