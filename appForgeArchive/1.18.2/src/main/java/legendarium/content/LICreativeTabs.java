164a6e9dbc7b97c76cb996a1565b91ed45e245a1
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-05 12:29:02 +0300

    1.18 and down go

--- appForgeArchive/1.18.2/src/main/java/legendarium/content/LICreativeTabs.java
+++ appForgeArchive/1.18.2/src/main/java/legendarium/content/LICreativeTabs.java
@@ -5,7 +5,7 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
 public class LICreativeTabs {
-	public static final CreativeModeTab TAB_ARTIFACTS = new CreativeModeTab("artifacts") {
+	public static final CreativeModeTab TAB_ARTIFACTS = new CreativeModeTab("legendarium.artifacts") {
 		@Override
 		public ItemStack makeIcon() {
 			return new ItemStack(LI.WEAPON_FARAMIR.get());
--- appForgeArchive/1.18.2/src/main/java/legendarium/model/LILargeItemModel.java
+++ appForgeArchive/1.18.2/src/main/java/legendarium/model/LILargeItemModel.java
@@ -13,49 +13,48 @@ import java.util.List;
 import java.util.Random;
 
 @SuppressWarnings("deprecation")
-public record LILargeItemModel(BakedModel smallModel, BakedModel largeModel) implements BakedModel {
-
+public record LILargeItemModel(BakedModel smallBakedModel, BakedModel largeBakedModel) implements BakedModel {
 	@Override
 	public BakedModel handlePerspective(ItemTransforms.TransformType transformType, PoseStack poseStack) {
-		var bakedModel = smallModel;
+		var bakedModel = smallBakedModel;
 		if (transformType == ItemTransforms.TransformType.FIRST_PERSON_LEFT_HAND || transformType == ItemTransforms.TransformType.FIRST_PERSON_RIGHT_HAND || transformType == ItemTransforms.TransformType.THIRD_PERSON_LEFT_HAND || transformType == ItemTransforms.TransformType.THIRD_PERSON_RIGHT_HAND) {
-			bakedModel = largeModel;
+			bakedModel = largeBakedModel;
 		}
 		return bakedModel.handlePerspective(transformType, poseStack);
 	}
 
 	@Override
 	public List<BakedQuad> getQuads(BlockState blockState, Direction direction, Random random) {
-		return smallModel.getQuads(blockState, direction, random);
+		return smallBakedModel.getQuads(blockState, direction, random);
 	}
 
 	@Override
 	public boolean useAmbientOcclusion() {
-		return smallModel.useAmbientOcclusion();
+		return smallBakedModel.useAmbientOcclusion();
 	}
 
 	@Override
 	public boolean isGui3d() {
-		return smallModel.isGui3d();
+		return smallBakedModel.isGui3d();
 	}
 
 	@Override
 	public boolean usesBlockLight() {
-		return smallModel.usesBlockLight();
+		return smallBakedModel.usesBlockLight();
 	}
 
 	@Override
 	public boolean isCustomRenderer() {
-		return smallModel.isCustomRenderer();
+		return smallBakedModel.isCustomRenderer();
 	}
 
 	@Override
 	public TextureAtlasSprite getParticleIcon() {
-		return smallModel.getParticleIcon();
+		return smallBakedModel.getParticleIcon();
 	}
 
 	@Override
 	public ItemOverrides getOverrides() {
-		return smallModel.getOverrides();
+		return smallBakedModel.getOverrides();
 	}
 }
