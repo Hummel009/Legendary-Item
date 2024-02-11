06aa688cb2ef789f6206a9a1ad94ae81793c3c41
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-11 20:51:05 +0300

    1

--- appNeoForge/src/main/java/legendarium/Main.java
+++ appNeoForge/src/main/java/legendarium/Main.java
@@ -1,7 +1,7 @@
 package legendarium;
 
 import legendarium.handler.ModEventHandler;
-import legendarium.init.CreativeTabs;
+import legendarium.init.ItemGroups;
 import legendarium.init.Items;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
@@ -15,6 +15,6 @@ public class Main {
 		modEventBus.register(modEventHandler);
 
 		Items.register(modEventBus);
-		CreativeTabs.register(modEventBus);
+		ItemGroups.register(modEventBus);
 	}
 }
--- appNeoForge/src/main/java/legendarium/init/ItemGroups.java
+++ appNeoForge/src/main/java/legendarium/init/ItemGroups.java
@@ -9,7 +9,7 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 @SuppressWarnings("WeakerAccess")
-public class CreativeTabs {
+public class ItemGroups {
 	private static final DeferredRegister<CreativeModeTab> CREATIVE_TABS = DeferredRegister.create(BuiltInRegistries.CREATIVE_MODE_TAB, "legendarium");
 
 	public static final DeferredHolder<CreativeModeTab, CreativeModeTab> TAB_ARTIFACTS = CREATIVE_TABS.register("artifacts", () -> CreativeModeTab.builder().title(Component.translatable("itemGroup.legendarium.artifacts")).icon(() -> new ItemStack(Items.WEAPON_FARAMIR.get())).displayItems((enabledFlags, populator) -> {
@@ -18,7 +18,7 @@ public class CreativeTabs {
 		}
 	}).build());
 
-	private CreativeTabs() {
+	private ItemGroups() {
 	}
 
 	public static void register(IEventBus modEventBus) {
