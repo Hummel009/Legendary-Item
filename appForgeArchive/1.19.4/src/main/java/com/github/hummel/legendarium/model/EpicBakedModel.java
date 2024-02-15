962e59b1f1c496fb75c1add86ff6b018a7114a8a
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-15 14:56:22 +0300

    19-fg

--- appForgeArchive/1.19.4/src/main/java/com/github/hummel/legendarium/model/EpicBakedModel.java
+++ appForgeArchive/1.19.4/src/main/java/com/github/hummel/legendarium/model/EpicBakedModel.java
@@ -3,6 +3,7 @@ package com.github.hummel.legendarium.model;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.ItemOverrides;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.Direction;
@@ -15,37 +16,19 @@ import java.util.List;
 @SuppressWarnings("deprecation")
 public record EpicBakedModel(BakedModel smallBakedModel, BakedModel largeBakedModel) implements BakedModel {
 	@Override
-	public BakedModel applyTransform(ItemDisplayContext itemDisplayContext, PoseStack poseStack, boolean b) {
-		var bakedModel = smallBakedModel;
+	public BakedModel applyTransform(ItemDisplayContext itemDisplayContext, PoseStack poseStack, boolean applyLeftHandTransform) {
+		BakedModel bakedModel;
 		if (itemDisplayContext == ItemDisplayContext.FIRST_PERSON_LEFT_HAND || itemDisplayContext == ItemDisplayContext.FIRST_PERSON_RIGHT_HAND || itemDisplayContext == ItemDisplayContext.THIRD_PERSON_LEFT_HAND || itemDisplayContext == ItemDisplayContext.THIRD_PERSON_RIGHT_HAND) {
 			bakedModel = largeBakedModel;
+		} else {
+			bakedModel = smallBakedModel;
 		}
-		return bakedModel.applyTransform(itemDisplayContext, poseStack, b);
+		return bakedModel.applyTransform(itemDisplayContext, poseStack, applyLeftHandTransform);
 	}
 
 	@Override
-	public List<BakedQuad> getQuads(BlockState blockState, Direction direction, RandomSource randomSource) {
-		return largeBakedModel.getQuads(blockState, direction, randomSource);
-	}
-
-	@Override
-	public boolean useAmbientOcclusion() {
-		return largeBakedModel.useAmbientOcclusion();
-	}
-
-	@Override
-	public boolean isGui3d() {
-		return largeBakedModel.isGui3d();
-	}
-
-	@Override
-	public boolean usesBlockLight() {
-		return largeBakedModel.usesBlockLight();
-	}
-
-	@Override
-	public boolean isCustomRenderer() {
-		return largeBakedModel.isCustomRenderer();
+	public ItemOverrides getOverrides() {
+		return largeBakedModel.getOverrides();
 	}
 
 	@Override
@@ -54,7 +37,32 @@ public record EpicBakedModel(BakedModel smallBakedModel, BakedModel largeBakedMo
 	}
 
 	@Override
-	public ItemOverrides getOverrides() {
-		return largeBakedModel.getOverrides();
+	public List<BakedQuad> getQuads(BlockState blockState, Direction direction, RandomSource randomSource) {
+		return largeBakedModel.getQuads(blockState, direction, randomSource);
+	}
+
+	@Override
+	public ItemTransforms getTransforms() {
+		return largeBakedModel.getTransforms();
+	}
+
+	@Override
+	public boolean isCustomRenderer() {
+		return largeBakedModel.isCustomRenderer();
+	}
+
+	@Override
+	public boolean isGui3d() {
+		return largeBakedModel.isGui3d();
+	}
+
+	@Override
+	public boolean useAmbientOcclusion() {
+		return largeBakedModel.useAmbientOcclusion();
+	}
+
+	@Override
+	public boolean usesBlockLight() {
+		return largeBakedModel.usesBlockLight();
 	}
 }
