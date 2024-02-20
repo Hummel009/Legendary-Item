bd105d10990f6f76fb60b199d861cb60ad5d533c
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-20 21:05:44 +0300

    predv

--- appForgeArchive/1.6.4/src/main/java/com/github/hummel/legendarium/proxy/ClientProxy.java
+++ appForgeArchive/1.6.4/src/main/java/com/github/hummel/legendarium/proxy/ClientProxy.java
@@ -1,17 +1,19 @@
 package com.github.hummel.legendarium.proxy;
 
-import com.github.hummel.legendarium.listener.ReloadListener;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.ReloadableResourceManager;
-import net.minecraft.client.resources.ResourceManager;
-import net.minecraft.client.resources.ResourceManagerReloadListener;
+import com.github.hummel.legendarium.init.Items;
+import com.github.hummel.legendarium.renderer.EpicItemRenderer;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.MinecraftForgeClient;
 
 public class ClientProxy implements CommonProxy {
 	@Override
 	public void onInit() {
-		ResourceManagerReloadListener reloadListener = new ReloadListener();
-		ResourceManager resourceManager = Minecraft.getMinecraft().getResourceManager();
-		reloadListener.onResourceManagerReload(resourceManager);
-		((ReloadableResourceManager) resourceManager).registerReloadListener(reloadListener);
+		for (Item item : Items.CONTENT) {
+			EpicItemRenderer epicItemRenderer = EpicItemRenderer.getRendererIfLarge(item);
+			System.out.println(epicItemRenderer);
+			if (epicItemRenderer != null) {
+				MinecraftForgeClient.registerItemRenderer(item.itemID, epicItemRenderer);
+			}
+		}
 	}
 }
--- appForgeArchive/1.6.4/src/main/java/com/github/hummel/legendarium/renderer/EpicItemRenderer.java
+++ appForgeArchive/1.6.4/src/main/java/com/github/hummel/legendarium/renderer/EpicItemRenderer.java
@@ -1,27 +1,25 @@
 package com.github.hummel.legendarium.renderer;
 
+import com.github.hummel.legendarium.Main;
 import com.google.common.base.CaseFormat;
-import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.client.TextureFXManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.client.renderer.texture.TextureManager;
-import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Icon;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.IItemRenderer;
 import org.lwjgl.opengl.GL11;
 
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
 public class EpicItemRenderer implements IItemRenderer {
 	private static final ResourceLocation ENCHANTMENT_TEXTURE = new ResourceLocation("textures/misc/enchanted_item_glint.png");
 	private static final Map<String, Float> SIZE_FOLDERS = new HashMap<String, Float>();
-	private static final Map<Item, Icon> LARGE_ICONS_MAP = new HashMap<Item, Icon>();
 
 	static {
 		SIZE_FOLDERS.put("large-2x", 2.0f);
@@ -29,129 +27,104 @@ public class EpicItemRenderer implements IItemRenderer {
 	}
 
 	private final Item item;
-	private final String folderName;
+	private final String folder;
 	private final float scale;
-	private Icon largeIcon;
 
-	private EpicItemRenderer(Item item, String folderName, float scale) {
+	private EpicItemRenderer(Item item, String folder, float scale) {
 		this.item = item;
-		this.folderName = folderName;
+		this.folder = folder;
 		this.scale = scale;
 	}
 
-	private static ResourceLocation getLargeTexturePath(Item item, String folder) {
-		String itemIconString = item.getUnlocalizedName().substring("item.".length());
-		itemIconString = CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, itemIconString);
-		GameRegistry.UniqueIdentifier uniqueIdentifier = GameRegistry.findUniqueIdentifierFor(item);
-		String modId = isNullOrEmpty(uniqueIdentifier.modId) ? "minecraft" : uniqueIdentifier.modId;
-		return new ResourceLocation(modId, "textures/items/" + folder + '/' + itemIconString + ".png");
+	@Override
+	public boolean handleRenderType(ItemStack itemstack, IItemRenderer.ItemRenderType type) {
+		return type == IItemRenderer.ItemRenderType.EQUIPPED || type == IItemRenderer.ItemRenderType.EQUIPPED_FIRST_PERSON;
+	}
+
+	@Override
+	public void renderItem(IItemRenderer.ItemRenderType type, ItemStack itemstack, Object... data) {
+		GL11.glPushMatrix();
+		GL11.glTranslatef(-(scale - 1.0f) / 2.0f, -(scale - 1.0f) / 2.0f, 0.0f);
+		GL11.glScalef(scale, scale, 1.0f);
+		TextureManager textureManager = Minecraft.getMinecraft().getTextureManager();
+		ResourceLocation itemResourceLocation = getItemResourceLocation(item, folder);
+		String itemTexturePath = getItemTexturePath(item, folder);
+		textureManager.bindTexture(itemResourceLocation);
+		int textureSize = TextureFXManager.instance().getTextureDimensions(itemTexturePath).width;
+		GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+		Tessellator tessellator = Tessellator.instance;
+		ItemRenderer.renderItemIn2D(tessellator, 1.0f, 0.0f, 0.0f, 1.0f, textureSize, textureSize, 0.0625F);
+		if (itemstack.hasEffect(0)) {
+			GL11.glDepthFunc(514);
+			GL11.glDisable(2896);
+			textureManager.bindTexture(ENCHANTMENT_TEXTURE);
+			GL11.glEnable(3042);
+			GL11.glBlendFunc(768, 1);
+			float shade = 0.76F;
+			GL11.glColor4f(0.5f * shade, 0.25F * shade, 0.8f * shade, 1.0f);
+			GL11.glMatrixMode(5890);
+			GL11.glPushMatrix();
+			float scale = 0.125F;
+			GL11.glScalef(scale, scale, scale);
+			float randomShift = Minecraft.getSystemTime() % 3000L / 3000.0f * 8.0f;
+			GL11.glTranslatef(randomShift, 0.0f, 0.0f);
+			GL11.glRotatef(-50.0f, 0.0f, 0.0f, 1.0f);
+			ItemRenderer.renderItemIn2D(tessellator, 0.0f, 0.0f, 1.0f, 1.0f, 256, 256, 0.0625F);
+			GL11.glPopMatrix();
+			GL11.glPushMatrix();
+			GL11.glScalef(scale, scale, scale);
+			randomShift = Minecraft.getSystemTime() % 4873L / 4873.0f * 8.0f;
+			GL11.glTranslatef(-randomShift, 0.0f, 0.0f);
+			GL11.glRotatef(10.0f, 0.0f, 0.0f, 1.0f);
+			ItemRenderer.renderItemIn2D(tessellator, 0.0f, 0.0f, 1.0f, 1.0f, 256, 256, 0.0625F);
+			GL11.glPopMatrix();
+			GL11.glMatrixMode(5888);
+			GL11.glDisable(3042);
+			GL11.glEnable(2896);
+			GL11.glDepthFunc(515);
+		}
+		GL11.glPopMatrix();
+	}
+
+	@Override
+	public boolean shouldUseRenderHelper(IItemRenderer.ItemRenderType type, ItemStack itemstack, IItemRenderer.ItemRendererHelper helper) {
+		return false;
 	}
 
 	public static EpicItemRenderer getRendererIfLarge(Item item) {
 		for (Map.Entry<String, Float> folder : SIZE_FOLDERS.entrySet()) {
-			float iconScale = folder.getValue();
 			try {
-				ResourceLocation resLoc = getLargeTexturePath(item, folder.getKey());
-				Minecraft.getMinecraft().getResourceManager().getResource(resLoc);
-				return new EpicItemRenderer(item, folder.getKey(), iconScale);
+				String itemTexturePath = getItemTexturePath(item, folder.getKey());
+				InputStream inputStream = null;
+				try {
+					inputStream = Main.class.getResourceAsStream(itemTexturePath);
+					if (inputStream != null) {
+						return new EpicItemRenderer(item, folder.getKey(), folder.getValue());
+					}
+				} finally {
+					try {
+						if (inputStream != null) {
+							inputStream.close();
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
 			} catch (Exception ignored) {
 			}
 		}
 		return null;
 	}
 
-	private static boolean isNullOrEmpty(String str) {
-		return str == null || str.isEmpty();
+	private static ResourceLocation getItemResourceLocation(Item item, String folder) {
+		return new ResourceLocation("legendarium:textures/items/" + folder + '/' + getItemName(item) + ".png");
 	}
 
-	private static void renderEnchantmentEffect() {
-		Tessellator tessellator = Tessellator.instance;
-		TextureManager textureManager = Minecraft.getMinecraft().getTextureManager();
-		GL11.glDepthFunc(514);
-		GL11.glDisable(2896);
-		textureManager.bindTexture(ENCHANTMENT_TEXTURE);
-		GL11.glEnable(3042);
-		GL11.glBlendFunc(768, 1);
-		float shade = 0.76F;
-		GL11.glColor4f(0.5f * shade, 0.25F * shade, 0.8f * shade, 1.0f);
-		GL11.glMatrixMode(5890);
-		GL11.glPushMatrix();
-		float scale = 0.125F;
-		GL11.glScalef(scale, scale, scale);
-		float randomShift = Minecraft.getSystemTime() % 3000L / 3000.0f * 8.0f;
-		GL11.glTranslatef(randomShift, 0.0f, 0.0f);
-		GL11.glRotatef(-50.0f, 0.0f, 0.0f, 1.0f);
-		ItemRenderer.renderItemIn2D(tessellator, 0.0f, 0.0f, 1.0f, 1.0f, 256, 256, 0.0625F);
-		GL11.glPopMatrix();
-		GL11.glPushMatrix();
-		GL11.glScalef(scale, scale, scale);
-		randomShift = Minecraft.getSystemTime() % 4873L / 4873.0f * 8.0f;
-		GL11.glTranslatef(-randomShift, 0.0f, 0.0f);
-		GL11.glRotatef(10.0f, 0.0f, 0.0f, 1.0f);
-		ItemRenderer.renderItemIn2D(tessellator, 0.0f, 0.0f, 1.0f, 1.0f, 256, 256, 0.0625F);
-		GL11.glPopMatrix();
-		GL11.glMatrixMode(5888);
-		GL11.glDisable(3042);
-		GL11.glEnable(2896);
-		GL11.glDepthFunc(515);
+	private static String getItemTexturePath(Item item, String folder) {
+		return "/assets/legendarium/textures/items/" + folder + '/' + getItemName(item) + ".png";
 	}
 
-	private void doTransformations() {
-		GL11.glTranslatef(-(scale - 1.0f) / 2.0f, -(scale - 1.0f) / 2.0f, 0.0f);
-		GL11.glScalef(scale, scale, 1.0f);
-	}
-
-	@Override
-	public boolean handleRenderType(ItemStack itemstack, ItemRenderType type) {
-		return type == ItemRenderType.EQUIPPED || type == ItemRenderType.EQUIPPED_FIRST_PERSON;
-	}
-
-	public void registerIcons(IconRegister register) {
-		largeIcon = registerLargeIcon(register, null);
-	}
-
-	private Icon registerLargeIcon(IconRegister register, String extra) {
-		String itemName = item.getUnlocalizedName().substring("item.".length());
-		itemName = CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, itemName);
-		GameRegistry.UniqueIdentifier uniqueIdentifier = GameRegistry.findUniqueIdentifierFor(item);
-		String modId = (isNullOrEmpty(uniqueIdentifier.modId) ? "minecraft" : uniqueIdentifier.modId) + ':';
-		String path = modId + folderName + '/' + itemName;
-		if (!isNullOrEmpty(extra)) {
-			path += '_' + extra;
-		}
-		Icon icon = register.registerIcon(path);
-		LARGE_ICONS_MAP.put(item, icon);
-		return icon;
-	}
-
-	@Override
-	public void renderItem(ItemRenderType type, ItemStack itemstack, Object... data) {
-		GL11.glPushMatrix();
-		renderLargeItem(itemstack);
-		if (itemstack.hasEffect(0)) {
-			renderEnchantmentEffect();
-		}
-		GL11.glPopMatrix();
-	}
-
-	private void renderLargeItem(Icon icon) {
-		doTransformations();
-		TextureManager textureManager = Minecraft.getMinecraft().getTextureManager();
-		textureManager.bindTexture(TextureMap.locationItemsTexture);
-		GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
-		Tessellator tess = Tessellator.instance;
-		ItemRenderer.renderItemIn2D(tess, icon.getMaxU(), icon.getMinV(), icon.getMinU(), icon.getMaxV(), icon.getIconWidth(), icon.getIconHeight(), 0.0625f);
-	}
-
-	private void renderLargeItem(ItemStack itemstack) {
-		if (largeIcon == null) {
-			largeIcon = LARGE_ICONS_MAP.get(itemstack.getItem());
-		}
-		renderLargeItem(largeIcon);
-	}
-
-	@Override
-	public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack itemstack, IItemRenderer.ItemRendererHelper helper) {
-		return false;
+	private static String getItemName(Item item) {
+		return CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, item.getUnlocalizedName().substring("item.".length()));
 	}
 }
