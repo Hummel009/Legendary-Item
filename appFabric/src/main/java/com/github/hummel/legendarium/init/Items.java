225ca8016637eade8b0c06a1c6f0917312d33e46
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-20 16:11:38 +0300

    content

--- appFabric/src/main/java/com/github/hummel/legendarium/init/Items.java
+++ appFabric/src/main/java/com/github/hummel/legendarium/init/Items.java
@@ -133,5 +133,6 @@ public class Items {
 
 	private static void register(Item item, String name) {
 		Registry.register(BuiltInRegistries.ITEM, new ResourceLocation("legendarium", name), item);
+		CONTENT.add(item);
 	}
 }
