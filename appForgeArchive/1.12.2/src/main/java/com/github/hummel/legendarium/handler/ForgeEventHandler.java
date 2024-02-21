a7c75fb3389869342c35b26451bba4b7e07d080c
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-21 15:09:50 +0300

    10-12 fix

--- appForgeArchive/1.12.2/src/main/java/com/github/hummel/legendarium/handler/ForgeEventHandler.java
+++ appForgeArchive/1.12.2/src/main/java/com/github/hummel/legendarium/handler/ForgeEventHandler.java
@@ -3,7 +3,6 @@ package com.github.hummel.legendarium.handler;
 import com.github.hummel.legendarium.Main;
 import com.github.hummel.legendarium.init.Items;
 import com.github.hummel.legendarium.model.EpicBakedModel;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.block.model.ModelBakery;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
@@ -11,6 +10,7 @@ import net.minecraft.item.Item;
 import net.minecraft.util.registry.IRegistry;
 import net.minecraftforge.client.event.ModelBakeEvent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
@@ -59,7 +59,7 @@ public class ForgeEventHandler {
 			}
 
 			ModelBakery.registerItemVariants(item, modelResourceLocations.toArray(new ModelResourceLocation[0]));
-			Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, smallModelResourceLocation);
+			ModelLoader.setCustomModelResourceLocation(item, 0, smallModelResourceLocation);
 		}
 	}
 
